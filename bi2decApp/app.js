56c3c95879990239f3828f599105f3ff22bd138c
astragenius <darthmalak86@gmail.com>
2022-01-09 21:03:34 +0100

    implemented RegEX for validating Binary numbers

--- bi2decApp/app.js
+++ bi2decApp/app.js
@@ -1,28 +1,45 @@
 
-const input = document.getElementById('input').value;
+const convert = document.getElementById('convert-btn');
+const reset = document.getElementById('reset-btn');
 
 
 
 
 
-function bintoDeci (value){
+
+function bintoDeci (){
     
+    const input = document.getElementById('input').value;
     let sum = [];
-    /* converting number to array */
-    let number = Array.from(String(value), Number);
-    
-    /* reverse iteration for loop */
-    for(let i = number.length -1; i >= 0; i--) {
-    
-       sum.push(number[i] * (Math.pow(2, [i])));
-        
-    }
-   
-    return sum = sum.reduce((x, y) => x + y);
+    const pattern = new RegExp('^[0-1]{1,}$');
 
     
+    console.log(pattern.test(input));
+    /* converting number to array */
+    if(pattern.test(input)) {
+        let number = Array.from(String(input), Number);
+        
+            
+                
+                /* reverse iteration for loop */
+                for(let i = number.length -1; i >= 0; i--) {
+                    
+                    sum.push(number[i] * (Math.pow(2, [i])));
+                    
+                }
+                /* Sum array values together */
+                
+                sum = sum.reduce((x, y) => x + y);
+                renderDom(sum);   
+    }else {
+        return;
+    }
     
 }
 
-console.log(bintoDeci(input));
+function renderDom(value) {
+    const output = document.getElementById('output');
+    output.innerText = value;  
+}
 
+convert.addEventListener('click', bintoDeci);
