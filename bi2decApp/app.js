627322db6250e5cd017b86a911673a845bd5fe6e
astragenius <darthmalak86@gmail.com>
2022-01-09 19:12:04 +0100

    working still on function

---
+++ bi2decApp/app.js
@@ -0,0 +1,26 @@
+
+const input = document.getElementById('input').value;
+
+
+
+
+
+function bintoDeci (value){
+    
+    let sum = [];
+    /* converting number to array */
+    let number = Array.from(String(value), Number);
+    
+    /* reverse iteration for loop */
+    for(let i = number.length -1; i >= 0; i--) {
+    
+       sum.push(number[i] * (Math.pow(2, [i])));
+        
+    }
+   return sum;
+    
+    
+}
+
+console.log(bintoDeci(input));
+
