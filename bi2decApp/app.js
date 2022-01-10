153bad5a1dda24c3e8e1340ae8c9f1f492c961df
astragenius <darthmalak86@gmail.com>
2022-01-10 20:44:38 +0100

    Styling input and output area.

--- bi2decApp/app.js
+++ bi2decApp/app.js
@@ -1,7 +1,8 @@
 
 const convert = document.getElementById('convert-btn');
 const reset = document.getElementById('reset-btn');
-const input = document.getElementById('input').value;
+const output1 = document.getElementById('output1');
+const output2 = document.getElementById('output2');
 
 
 
@@ -10,31 +11,38 @@ const input = document.getElementById('input').value;
 
 function bintoDeci (){
     
+    const input = document.getElementById('input').value;
     let sum = [];
     const pattern = new RegExp('^[0-1]{1,}$');
-
+    console.log(input);
     /* converting number to array */
     if(pattern.test(input)) {
         let number = Array.from(String(input), Number);
+        
+        /* reverse iteration for loop */
+        for(let i = number.length -1; i >= 0; i--) {
             
-                /* reverse iteration for loop */
-                for(let i = number.length -1; i >= 0; i--) {
-                    
-                    sum.push(number[i] * (Math.pow(2, [i]))); 
-                }
-                /* Sum array values together */         
-                sum = sum.reduce((x, y) => x + y);
-                renderDom(sum);   
+            sum.push(number[i] * (Math.pow(2, [i]))); 
+        }
+        /* Sum array values together */         
+        sum = sum.reduce((x, y) => x + y);
+        renderDom(sum, input);   
+        
     }else {
         return;
     }
+
     
 }
 
-function renderDom(value) {
-    const output = document.getElementById('output');
-    output.innerText = value;  
+function renderDom(value, input) {
+    output1.innerText = input;
+    output2.innerText = value;
+    
 }
 
 convert.addEventListener('click', bintoDeci);
-reset.addEventListener('click', () => {input = "";})
+reset.addEventListener('click', () => {
+   
+    
+})
