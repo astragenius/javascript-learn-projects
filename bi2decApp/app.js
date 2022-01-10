dab5473f45398458dfb061dfb3045a0197543b49
astragenius <darthmalak86@gmail.com>
2022-01-10 19:18:53 +0100

    Adding markup and styling to the form.

--- bi2decApp/app.js
+++ bi2decApp/app.js
@@ -1,6 +1,7 @@
 
 const convert = document.getElementById('convert-btn');
 const reset = document.getElementById('reset-btn');
+const input = document.getElementById('input').value;
 
 
 
@@ -9,26 +10,19 @@ const reset = document.getElementById('reset-btn');
 
 function bintoDeci (){
     
-    const input = document.getElementById('input').value;
     let sum = [];
     const pattern = new RegExp('^[0-1]{1,}$');
 
-    
-    console.log(pattern.test(input));
     /* converting number to array */
     if(pattern.test(input)) {
         let number = Array.from(String(input), Number);
-        
             
-                
                 /* reverse iteration for loop */
                 for(let i = number.length -1; i >= 0; i--) {
                     
-                    sum.push(number[i] * (Math.pow(2, [i])));
-                    
+                    sum.push(number[i] * (Math.pow(2, [i]))); 
                 }
-                /* Sum array values together */
-                
+                /* Sum array values together */         
                 sum = sum.reduce((x, y) => x + y);
                 renderDom(sum);   
     }else {
@@ -43,3 +37,4 @@ function renderDom(value) {
 }
 
 convert.addEventListener('click', bintoDeci);
+reset.addEventListener('click', () => {input = "";})
