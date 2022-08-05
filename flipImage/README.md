4af79155cf48b33ba1e0e7eb5e611c7e0696fe36
astragenius <darthmalak86@gmail.com>
2022-08-05 18:45:37 +0200

    add - added getNewImage function to get a image

--- flipImage/README.md
+++ flipImage/README.md
@@ -16,11 +16,11 @@ Image packages and libraries are not allowed.
 
 ## User Stories
 
--   [ ] User can see a pane containing a single image repeated in a 2x2 matrix
--   [ ] User can flip any one of the images vertically or horizontally using a set of up, down, left, and right arrows next to the image
+-   [x] User can see a pane containing a single image repeated in a 2x2 matrix
+-   [x] User can flip any one of the images vertically or horizontally using a set of up, down, left, and right arrows next to the image
 
 ## Bonus features
 
--   [ ] User can change the default image by entering the URL of a different image in an input field
--   [ ] User can display the new image by clicking a 'Display' button next to the input field
+-   [x] User can change the default image by entering the URL of a different image in an input field
+-   [x] User can display the new image by clicking a 'Display' button next to the input field
 -   [ ] User can see an error message if the new images URL is not found
--- flipImage/app.js
+++ flipImage/app.js
@@ -11,22 +11,41 @@ const btnLeft = document.getElementById('left')
 
 
 function rotateLeft() {
-    image.style.animation = 'flip-with-scale'
-    image.style.animationDuration = '2s'
+    image.style.animation = 'flip-left'
+    image.style.animationTimingFunction = 'cubic-bezier(0.86, -0.1, 0.27, 1.15)'
+    image.style.animationDuration = '1s'
     image.style.transform = 'scaleX(-1)'
 
 }
 
 function rotateRight() {
+    image.style.animation = 'flip-right'
+    image.style.animationTimingFunction = 'cubic-bezier(0.86, -0.1, 0.27, 1.15)'
+    image.style.animationDuration = '1s'
     image.style.transform = 'scaleX(1)'
 }
 
 function rotateUp() {
+    image.style.animation = 'flip-up'
+    image.style.animationTimingFunction = 'cubic-bezier(0.86, -0.1, 0.27, 1.15)'
+    image.style.animationDuration = '1s'
     image.style.transform = 'scaleY(-1)'
 }
 
 function rotateDown() {
+    image.style.animation = 'flip-down'
+    image.style.animationTimingFunction = 'cubic-bezier(0.86, -0.1, 0.27, 1.15)'
+    image.style.animationDuration = '1s'
     image.style.transform = 'scaleY(1)';
+}
+
+function getNewImage() {
+    let newImage = input.value
+    image.src = "";
+    image.src = newImage;
+
+   
+
 }
 
 
@@ -34,3 +53,5 @@ btnLeft.addEventListener('click', rotateLeft);
 btnRight.addEventListener('click', rotateRight);
 btnUp.addEventListener('click', rotateUp);
 btndown.addEventListener('click', rotateDown)
+
+displayBtn.addEventListener('click', getNewImage);
